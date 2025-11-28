bab268a113812d448101c76a60392f2d65bab4d8
MDZAHIDHASAN2024 <zahidbinanwar3@gmail.com>
2025-11-28 10:19:03 +0600

    version 6.07

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="container-fluid bg-dark text-light text-center ">
-      <p> &#9400; All Rights Reserved 2025 Developed by Zahid (V.6.06) </p>
+      <p> &#9400; All Rights Reserved 2025 Developed by Zahid (V.6.07) </p>
     </div>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -194,10 +194,10 @@ const Home = () => {
             </button>
             {toggle && (
               <div className=" bg-dark text-light text-center  p-1">
-                <p>26/11/2025 English</p>
-                <p>11/08/1432 Bangla</p>
-                <p>04/06/1447 Arabic</p>
-                <p>Wednesday</p>
+                <p>28/11/2025 English</p>
+                <p>13/08/1432 Bangla</p>
+                <p>06/06/1447 Arabic</p>
+                <p>Friday</p>
               </div>
             )}
           </div>
