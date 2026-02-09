c6bed63c3b3bdccbe20c82e69b66b2c015effb76
MDZAHIDHASAN2024 <zahidbinanwar3@gmail.com>
2026-02-09 21:46:41 +0600

    version 11.03

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -194,10 +194,10 @@ const Home = () => {
             </button>
             {toggle && (
               <div className=" bg-dark text-light text-center  p-1">
-                <p>09/02/2026 English</p>
-                <p>26/10/1432 Bangla</p>
-                <p>20/08/1447 Arabic</p>
-                <p>Monday</p>
+                <p>10/02/2026 English</p>
+                <p>27/10/1432 Bangla</p>
+                <p>21/08/1447 Arabic</p>
+                <p>Tuesday</p>
               </div>
             )}
           </div>
